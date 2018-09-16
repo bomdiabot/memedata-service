cc6fa0386e2ac0356ef882dfa155008545dbcd10
Erik Perillo <erik.perillo@gmail.com>
2018-09-16 04:09:20 -0300

    script to publish docs to gh-pages branch created

---
+++ publish_docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+main_branch="dev"
+
+git checkout gh-pages
+git rm -rf .
+
+git checkout $main_branch docs/
+git checkout $main_branch memedata/
+(cd docs && make html)
+
+mv docs/build/html/* .
+rm -rf docs memedata publish_docs.sh
+
+git add -A
+git commit -m \
+    "gen docs from `git log $main_branch -1 --pretty=short --abbrev-commit`"
+git push origin gh-pages
+
+git checkout dev
